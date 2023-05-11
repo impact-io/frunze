088e5c40e37718776c701110b22248f28417b7e6
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-11 13:50:50 +0600

    test

--- shop/models.py
+++ shop/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=100, blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-
 
 class Product(models.Model):
     name = models.CharField(max_length=100, blank=True)
@@ -18,17 +12,6 @@ class Product(models.Model):
     is_active = models.BooleanField(default=True)
     uploaded = models.DateTimeField(auto_now=True, verbose_name='Изменен')
     created_at = models.DateField(auto_now=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
-
-
-class Order(models.Model):
-    ordered_items = models.ForeignKey(Product, on_delete=models.CASCADE)
-    total_price = models.FloatField(verbose_name="jalpy baasy")
-    created_at = models.DateField(auto_now=True)
-    phone = models.CharField(max_length=100, blank=True)
-
-    def __str__(self):
-        return self.phone
--- shop/views.py
+++ shop/views.py
@@ -1,29 +1,15 @@
 from django.http import HttpResponse
 from rest_framework import viewsets
 from rest_framework import permissions
-from .serializers import CategorySerializer, ProductSerializer, OrderSerializer
-from .models import Product, Order
+from .serializers import  ProductSerializer
+from .models import Product
 from rest_framework.response import Response
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.filter()
     serializer_class = ProductSerializer
-    # permission_classes = [permissions.IsAuthenticated]
-
-class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
-
-class CategoryViewSet(viewsets.ModelViewSet):
-    serializer_class = CategorySerializer
-    queryset = Product.objects.all()
-    
-    def list(self, request, *args, **kwargs):
-        cat = self.kwargs['cat_id']
-        queryset = Product.objects.filter(category = cat)
-        serializer = CategorySerializer(queryset, many=True)
-        return Response(serializer.data)
+    # permission_classes = [permissions.IsAuthenticated
     
 
 class MainViewSet(viewsets.ModelViewSet):
