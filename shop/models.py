5104a56cf96411ecb289a00f8637105964bebc94
Janzat <janzatnurjigitova@gmail.com>
2023-05-13 11:53:33 +0600

    text2

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -12,6 +18,7 @@ class Product(models.Model):
     is_active = models.BooleanField(default=True)
     uploaded = models.DateTimeField(auto_now=True, verbose_name='Изменен')
     created_at = models.DateField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from rest_framework import viewsets
 from rest_framework import permissions
-from .serializers import  ProductSerializer
+from .serializers import  ProductSerializer,CategorySerializer
 from .models import Product
 from rest_framework.response import Response
 
@@ -11,6 +11,16 @@ class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     # permission_classes = [permissions.IsAuthenticated
     
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Product.objects.all()
+    
+    def list(self, request, *args, **kwargs):
+        cat = self.kwargs['cat_id']
+        queryset = Product.objects.filter(category = cat)
+        serializer = CategorySerializer(queryset, many=True)
+        return Response(serializer.data)
+
 
 class MainViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
